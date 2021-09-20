6e99414c7d864d6bdfa0d3ac18462d99acbb140c
jhhong0930 <jhhong0930@gmail.com>
2021-09-20 20:06:03 +0900

    JPA - 생성일자, 수정일자

--- week02/src/main/java/com/sparta/week02/Week02Application.java
+++ week02/src/main/java/com/sparta/week02/Week02Application.java
@@ -1,13 +1,37 @@
 package com.sparta.week02;
 
+import com.sparta.week02.domain.Course;
+import com.sparta.week02.domain.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.List;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Week02Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Week02Application.class, args);
     }
 
+    // Week02Application.java 의 main 함수 아래에 붙여주세요.
+    @Bean
+    public CommandLineRunner demo(CourseRepository repository) {
+        return (args) -> {
+
+            Course course1 = new Course("웹개발의 봄, Spring", "남병관");
+
+            repository.save(course1);
+
+            List<Course> courseList = repository.findAll();
+
+            for (Course c : courseList) {
+                System.out.println(c);
+            }
+        };
+    }
 }
