80c359967663ee0f19e63d7724920c42c5af1159
jhhong0930 <jhhong0930@gmail.com>
2021-09-21 17:53:50 +0900

    관심 상품 조회하기

--- week04/src/main/java/com/sparta/week04/Week04Application.java
+++ week04/src/main/java/com/sparta/week04/Week04Application.java
@@ -2,8 +2,10 @@ package com.sparta.week04;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Week04Application {
 
 	public static void main(String[] args) {
