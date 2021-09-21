56755df938f0e7b3041858eb81ca30c40a7f3f1e
jhhong0930 <jhhong0930@gmail.com>
2021-09-21 10:54:20 +0900

    domain, service, controller 생성 & CRUD 테스트

--- week03/src/main/java/com/sparta/week03/Week03Application.java
+++ week03/src/main/java/com/sparta/week03/Week03Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Week03Application {
 
     public static void main(String[] args) {
