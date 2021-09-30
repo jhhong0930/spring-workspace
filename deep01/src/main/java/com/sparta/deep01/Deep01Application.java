ba078d85d74ac7f07bf153add81cd327a0a5f733
jhhong0930 <jhhong0930@gmail.com>
2021-09-30 10:43:59 +0900

    create project deep01

---
+++ deep01/src/main/java/com/sparta/deep01/Deep01Application.java
@@ -0,0 +1,13 @@
+package com.sparta.deep01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deep01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Deep01Application.class, args);
+    }
+
+}
---
+++ deep01/src/test/java/com/sparta/deep01/Deep01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.deep01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deep01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
