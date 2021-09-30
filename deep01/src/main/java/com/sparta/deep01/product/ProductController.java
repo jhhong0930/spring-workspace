b31e499647aa94db688fa411e7542389ddb88472
jhhong0930 <jhhong0930@gmail.com>
2021-09-30 16:27:18 +0900

    객체 중복 생성 문제 해결

--- deep01/src/main/java/com/sparta/deep01/product/ProductController.java
+++ deep01/src/main/java/com/sparta/deep01/product/ProductController.java
@@ -13,10 +13,16 @@ import java.util.List;
 @RestController // JSON으로 데이터를 주고받음을 선언합니다.
 public class ProductController {
 
+    private final ProductService productService;
+
+    public ProductController() {
+        ProductService productService = new ProductService();
+        this.productService = productService;
+    }
+
     // 신규 상품 등록
     @PostMapping("/api/products")
     public Product createProduct(@RequestBody ProductRequestDto requestDto) throws SQLException {
-        ProductService productService = new ProductService();
         Product product = productService.createProduct(requestDto);
 
         // 응답 보내기
@@ -26,7 +32,6 @@ public class ProductController {
     // 설정 가격 변경
     @PutMapping("/api/products/{id}")
     public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) throws SQLException {
-        ProductService productService = new ProductService();
         Product product = productService.updateProduct(id, requestDto);
 
         // 응답 보내기 (업데이트된 상품 id)
@@ -36,7 +41,6 @@ public class ProductController {
     // 등록된 전체 상품 목록 조회
     @GetMapping("/api/products")
     public List<Product> getProducts() throws SQLException {
-        ProductService productService = new ProductService();
         List<Product> products = productService.getProducts();
 
         // 응답 보내기
--- deep01/src/main/java/com/sparta/deep01/product/ProductService.java
+++ deep01/src/main/java/com/sparta/deep01/product/ProductService.java
@@ -7,20 +7,28 @@ import com.sparta.deep01.domain.ProductRequestDto;
 import java.sql.SQLException;
 import java.util.List;
 
+import java.sql.SQLException;
+import java.util.List;
+
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService() {
+        ProductRepository productRepository = new ProductRepository();
+        this.productRepository = productRepository;
+    }
 
     public Product createProduct(ProductRequestDto requestDto) throws SQLException {
         // 요청받은 DTO 로 DB에 저장할 객체 만들기
         Product product = new Product(requestDto);
 
-        ProductRepository productRepository = new ProductRepository();
         productRepository.createProduct(product);
 
         return product;
     }
 
     public Product updateProduct(Long id, ProductMypriceRequestDto requestDto) throws SQLException {
-        ProductRepository productRepository = new ProductRepository();
         Product product = productRepository.getProduct(id);
         if (product == null) {
             throw new NullPointerException("해당 아이디가 존재하지 않습니다.");
@@ -33,7 +41,6 @@ public class ProductService {
     }
 
     public List<Product> getProducts() throws SQLException {
-        ProductRepository productRepository = new ProductRepository();
         List<Product> products = productRepository.getProducts();
 
         return products;
