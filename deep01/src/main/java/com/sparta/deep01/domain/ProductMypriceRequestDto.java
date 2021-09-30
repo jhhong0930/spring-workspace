2027ea14f9a3cf0099a79a7776ab31b180c0f51f
jhhong0930 <jhhong0930@gmail.com>
2021-09-30 15:29:32 +0900

    관심상품 등록 API 구현하기

---
+++ deep01/src/main/java/com/sparta/deep01/domain/ProductMypriceRequestDto.java
@@ -0,0 +1,12 @@
+package com.sparta.deep01.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ProductMypriceRequestDto {
+    private int myprice;
+}
