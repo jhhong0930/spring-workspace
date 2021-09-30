03f7985af618819aae7e15f34792bbe9520d384d
jhhong0930 <jhhong0930@gmail.com>
2021-09-30 17:14:38 +0900

    스프링 3계층 Annotation 적용하기

--- deep01/src/main/java/com/sparta/deep01/product/ProductService.java
+++ deep01/src/main/java/com/sparta/deep01/product/ProductService.java
@@ -17,29 +17,30 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-    public Product createProduct(ProductRequestDto requestDto) throws SQLException {
+    public Product createProduct(ProductRequestDto requestDto) {
         // 요청받은 DTO 로 DB에 저장할 객체 만들기
         Product product = new Product(requestDto);
 
-        productRepository.createProduct(product);
+        productRepository.save(product);
 
         return product;
     }
 
-    public Product updateProduct(Long id, ProductMypriceRequestDto requestDto) throws SQLException {
-        Product product = productRepository.getProduct(id);
-        if (product == null) {
-            throw new NullPointerException("해당 아이디가 존재하지 않습니다.");
-        }
+    public Product updateProduct(Long id, ProductMypriceRequestDto requestDto) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(
+                        () -> new NullPointerException("해당 아이디가 존재하지 않습니다.")
+                );
 
         int myprice = requestDto.getMyprice();
-        productRepository.updateMyprice(id, myprice);
+        product.setMyprice(myprice);
+        productRepository.save(product);
 
         return product;
     }
 
-    public List<Product> getProducts() throws SQLException {
-        List<Product> products = productRepository.getProducts();
+    public List<Product> getProducts() {
+        List<Product> products = productRepository.findAll();
 
         return products;
     }
