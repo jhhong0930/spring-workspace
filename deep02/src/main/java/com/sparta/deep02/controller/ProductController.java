d0ccaf43388158006018ca6f68473227cb1285b0
jhhong0930 <jhhong0930@gmail.com>
2021-10-01 17:21:06 +0900

    회원별 상품 등록 및 조회

--- deep02/src/main/java/com/sparta/deep02/controller/ProductController.java
+++ deep02/src/main/java/com/sparta/deep02/controller/ProductController.java
@@ -3,23 +3,32 @@ package com.sparta.deep02.controller;
 import com.sparta.deep02.domain.Product;
 import com.sparta.deep02.dto.ProductMypriceRequestDto;
 import com.sparta.deep02.dto.ProductRequestDto;
+import com.sparta.deep02.security.UserDetailsImpl;
 import com.sparta.deep02.service.ProductService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.SQLException;
 import java.util.List;
 
-@RequiredArgsConstructor // final로 선언된 멤버 변수를 자동으로 생성합니다.
 @RestController // JSON으로 데이터를 주고받음을 선언합니다.
 public class ProductController {
 
     private final ProductService productService;
 
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     // 신규 상품 등록
     @PostMapping("/api/products")
-    public Product createProduct(@RequestBody ProductRequestDto requestDto) throws SQLException {
-        Product product = productService.createProduct(requestDto);
+    public Product createProduct(@RequestBody ProductRequestDto requestDto,
+                                 @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        // 로그인 되어 있는 회원 테이블의 ID
+        Long userId = userDetails.getUser().getId();
+
+        Product product = productService.createProduct(requestDto, userId);
 
         // 응답 보내기
         return product;
@@ -27,19 +36,19 @@ public class ProductController {
 
     // 설정 가격 변경
     @PutMapping("/api/products/{id}")
-    public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) throws SQLException {
+    public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
         Product product = productService.updateProduct(id, requestDto);
 
         // 응답 보내기 (업데이트된 상품 id)
         return product.getId();
     }
 
-    // 등록된 전체 상품 목록 조회
+    // 로그인한 회원이 등록한 관심 상품 조회
     @GetMapping("/api/products")
-    public List<Product> getProducts() throws SQLException {
-        List<Product> products = productService.getProducts();
+    public List<Product> getProducts(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        // 로그인 되어 있는 회원 테이블의 ID
+        Long userId = userDetails.getUser().getId();
 
-        // 응답 보내기
-        return products;
+        return productService.getProducts(userId);
     }
 }
--- deep02/src/main/java/com/sparta/deep02/repository/ProductRepository.java
+++ deep02/src/main/java/com/sparta/deep02/repository/ProductRepository.java
@@ -1,6 +1,11 @@
 package com.sparta.deep02.repository;
 
 import com.sparta.deep02.domain.Product;
+import com.sparta.deep02.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> { }
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findAllByUserId(Long userId);
+}
