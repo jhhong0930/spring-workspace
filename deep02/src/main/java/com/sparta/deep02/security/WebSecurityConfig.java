897103a257128c26e803532d22b1d879f17ba1ce
jhhong0930 <jhhong0930@gmail.com>
2021-10-01 15:26:07 +0900

    update: 코드 수정

--- deep02/src/main/java/com/sparta/deep02/security/WebSecurityConfig.java
+++ deep02/src/main/java/com/sparta/deep02/security/WebSecurityConfig.java
@@ -26,9 +26,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // 회원 관리 처리 API (POST /user/**) 에 대해 CSRF 무시
-        http.csrf()
-                .ignoringAntMatchers("/user/**");
+        // 모든 csrf 비활성화
+        http.csrf().disable();
 
         http.authorizeRequests()
                 // image 폴더를 login 없이 허용
@@ -40,15 +39,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 // 그 외 어떤 요청이든 '인증'
                 .anyRequest().authenticated()
                 .and()
-                    // 로그인 기능
+                    // [로그인 기능]
                     .formLogin()
+                    // 로그인 View 제공 (GET /user/login)
                     .loginPage("/user/login")
+                    // 로그인 처리 (POST /user/login)
+                    .loginProcessingUrl("/user/login")
+                    // 로그인 처리 후 성공 시 URL
                     .defaultSuccessUrl("/")
+                    // 로그인 처리 후 실패 시 URL
                     .failureUrl("/user/login?error")
                     .permitAll()
                 .and()
-                    // 로그아웃 기능
+                    // [로그아웃 기능]
                     .logout()
+                    // 로그아웃 처리 URL
+                    .logoutUrl("/user/logout")
                     .permitAll();
     }
 }
