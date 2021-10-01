c79d8b68a20bc381079f0d257045d790674a6e64
jhhong0930 <jhhong0930@gmail.com>
2021-10-01 17:36:57 +0900

    관리자 상품 조회

--- deep02/src/main/java/com/sparta/deep02/controller/HomeController.java
+++ deep02/src/main/java/com/sparta/deep02/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.sparta.deep02.controller;
 
+
+import com.sparta.deep02.domain.UserRoleEnum;
 import com.sparta.deep02.security.UserDetailsImpl;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -11,6 +13,11 @@ public class HomeController {
     @GetMapping("/")
     public String home(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         model.addAttribute("username", userDetails.getUsername());
+
+        if (userDetails.getUser().getRole() == UserRoleEnum.ADMIN) {
+            model.addAttribute("admin_role", true);
+        }
+
         return "index";
     }
 }
