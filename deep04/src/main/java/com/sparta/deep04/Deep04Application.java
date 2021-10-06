708606525e20e287b1b757138a7b6c910bd62f07
jhhong0930 <jhhong0930@gmail.com>
2021-10-06 10:25:15 +0900

    create project deep04

---
+++ deep04/src/main/java/com/sparta/deep04/Deep04Application.java
@@ -0,0 +1,13 @@
+package com.sparta.deep04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deep04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Deep04Application.class, args);
+    }
+
+}
---
+++ deep04/src/test/java/com/sparta/deep04/Deep04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.deep04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deep04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
